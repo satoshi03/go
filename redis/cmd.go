e557e842161d8581cee226a32da85cb0d8b6510e
satoshi03 <innamisatoshi@gmail.com>
2016-09-17 17:12:10 +0900

    impl handling  packed value

--- redis/cmd.go
+++ redis/cmd.go
@@ -3,14 +3,24 @@ package redis
 import (
 	rlib "github.com/garyburd/redigo/redis"
 	"golang.org/x/net/context"
+	msgpack "gopkg.in/vmihailenco/msgpack.v2"
 )
 
-func SetCmd(ctx context.Context, redisname, key, val string) (string, error) {
+func SetString(ctx context.Context, redisname, key, val string) (string, error) {
 	rconn := GetWriteConn(ctx, redisname)
 	return rlib.String(rconn.Do("SET", key, val))
 }
 
-func GetCmd(ctx context.Context, redisname, key string) (string, error) {
+func GetString(ctx context.Context, redisname, key string) (string, error) {
 	rconn := GetReadConn(ctx, redisname)
 	return rlib.String(rconn.Do("GET", key))
+}
+
+func GetPackedValue(ctx context.Context, redisname, key string, out interface{}) interface{} {
+	rconn := GetReadConn(ctx, redisname)
+	value, _ := rlib.Bytes(rconn.Do("GET", key))
+	if err := msgpack.Unmarshal(value, out); err != nil {
+		return nil
+	}
+	return out
 }
